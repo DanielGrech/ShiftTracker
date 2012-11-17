102b7ca42b5d3ad6983ff1abcb67a5f2d741cfff
Daniel Grech <danielgrech91@gmail.com>
2012-11-17 15:51:03 +1100

    Moved export to common + fixed bug of no title for default reminders

--- ShiftTracker_paid/src/com/dgsd/android/ShiftTracker/StPaidApp.java
+++ ShiftTracker_paid/src/com/dgsd/android/ShiftTracker/StPaidApp.java
@@ -38,24 +38,6 @@ public class StPaidApp extends StApp implements DbService.OnDbEventListener, Set
 
     @Override
     public void onSettingsCreated(final PreferenceActivity activity, PreferenceManager prefsManager, PreferenceScreen screen) {
-        PreferenceCategory dataCategory = new PreferenceCategory(activity);
-        dataCategory.setTitle(R.string.settings_category_data);
-        dataCategory.setOrder(2);
-        screen.addPreference(dataCategory);
-
-        Preference exportPref = new Preference(activity);
-        exportPref.setTitle(R.string.settings_title_export);
-        exportPref.setSummary(R.string.settings_summary_export);
-        exportPref.setKey(getString(R.string.settings_key_export));
-        exportPref.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
-            @Override
-            public boolean onPreferenceClick(Preference preference) {
-                ExportDataTask.start(activity);
-
-                return true;
-            }
-        });
-        dataCategory.addPreference(exportPref);
 
         PreferenceCategory defaultsCateogry = (PreferenceCategory) screen.findPreference(getString(R.string.settings_category_defaults));
         Preference remindersPref = null;
@@ -63,10 +45,12 @@ public class StPaidApp extends StApp implements DbService.OnDbEventListener, Set
             remindersPref = new android.preference.ListPreference(activity);
             ((android.preference.ListPreference)remindersPref).setEntries(R.array.reminder_minutes_labels);
             ((android.preference.ListPreference)remindersPref).setEntryValues(R.array.reminder_minutes_labels);
+            ((android.preference.ListPreference)remindersPref).setDialogTitle(R.string.settings_title_default_reminder);
         } else {
             remindersPref = new ListPreference(activity);
             ((ListPreference)remindersPref).setEntries(R.array.reminder_minutes_labels);
             ((ListPreference)remindersPref).setEntryValues(R.array.reminder_minutes_labels);
+            ((ListPreference)remindersPref).setDialogTitle(R.string.settings_title_default_reminder);
         }
 
         CharSequence val = Prefs.getInstance(this).get(getString(R.string.settings_key_default_reminder), null);
